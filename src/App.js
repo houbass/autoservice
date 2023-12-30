7f95a2e9e6f78b7924b83da621db75f577f4afd8
houbass <laube.ondrej@seznam.cz>
2023-12-30 21:33:13 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react, { useRef } from 'react';
+import react, { useEffect, useRef, useState } from 'react';
 
 //style
 import './App.css';
@@ -18,6 +18,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Map from './components/Map';
 import Galery from './components/Galery';
+import About from './components/About';
 import Footer from './components/Footer';
 
 
@@ -27,16 +28,70 @@ function App() {
   const main = useRef();
   const kontakt = useRef();
   const galery = useRef();
+  const about = useRef();
+
+  const [allImagesLoaded, setAllImagesLoaded] = useState(false);
+  const [mainOpacity, setMainOpacity] = useState(0);
+  const [introAnimClass, setIntroAnimClass] = useState(["textPathDefault", "introUnderlineDefault"]);
+
+  //IMAGES
+  const imageUrls = [ToolPic, DiagnosticPic, KlimaPic, SkloPic, PneuservisPic, AutoservisPic];
+
+  //WAIT TO LOAD ALL IMAGES
+  useEffect(() => {
+    const loadImage = (url) =>
+      new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = reject;
+        img.src = url;
+      });
+
+    const loadAllImages = async () => {
+      try {
+        const imagePromises = imageUrls.map(loadImage);
+        await Promise.all(imagePromises);
+        setAllImagesLoaded(true);
+      } catch (error) {
+        console.error('Error loading images:', error);
+      }
+    };
+
+    loadAllImages();
+  }, []);
+
+  //CHECK IF ALL IMAGES ARE LOADED
+  useEffect(() => {
+    if(allImagesLoaded === true){
+      setMainOpacity(1);
+
+      setTimeout(() => {
+        setIntroAnimClass(["textPath", "introUnderline"])
+      }, 700)
+    }
+  }, [allImagesLoaded])
+
   
   return (
-    <div className="App">
+    <div 
+    className="App"
+    style={{
+      opacity: mainOpacity,
+      transition: "2s"
+    }}
+    >
       <Navbar 
       header={header}
       main={main}
       kontakt={kontakt} 
-      galery={galery}
+      galery={galery} 
+      about={about}
       />
-      <Header header={header} ToolPic={ToolPic}/>
+      <Header 
+      header={header} 
+      ToolPic={ToolPic}
+      introAnimClass={introAnimClass}
+      />
       <Main 
       main={main}
       DiagnosticPic={DiagnosticPic} 
@@ -48,6 +103,7 @@ function App() {
       />
       <Map kontakt={kontakt}/>
       <Galery galery={galery}/>
+      <About about={about}/>
       <Footer />
     </div>
   );
